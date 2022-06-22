d4ab92fe8a0ced8aea2fff839c19e1ecfbc93d8a
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-06-22 16:15:21 +0200

    correction for step 1 to 3

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CoordinateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,5 +14,22 @@ class GameController extends AbstractController
     public function index(): Response
     {
         return $this->render('game/index.html.twig');
+    }
+
+    #[Route('/bombify/{x<\d+>}/{y<\d+>}', name: 'bomb_boat')]
+    public function bomb(
+        int $x, 
+        int $y, 
+        CoordinateRepository $coordinateRepository): Response
+    {
+        $coordinate = $coordinateRepository->findOneBy(['x' => $x, 'y' => $y]);
+
+        if($coordinate) {
+            $this->addFlash('success', 'bateau touché !');
+        } else {
+            $this->addFlash('warning', 'coup manqué...');
+        }
+
+        return $this->redirectToRoute('app_game');
     }
 }
